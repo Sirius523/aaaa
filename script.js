ad30c4eaaf9f9a26e8ae06ca2890cf4568a8ebf6
Sirius523 <82580604+Sirius523@users.noreply.github.com>
2022-10-11 10:46:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+ //153(1)
+//for (var i = 0; i < 3; i++){
+  //  $("h1").slideUp(500);
+    //$("h1").slideDown(500);
+
+
+
+//}
+
+//155(4)
+//$("#main-heading").text("Мои друзья");
+//"h1".fodeTo (2000, 0.5)
+//var names = ["Давид", "Данил", "Артем"];
+//for (var i = 0; i < names.length; i++) {
+ //   $("body").append("<p>" + names[i] + "</p>");
+ //   $("p").last().hide().fadeIn((i + 1) * 10000);
+//}
+//$("p").append(" лучший!");
+
+
+
+
